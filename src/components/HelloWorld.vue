1647baf8cd91b746ba22ad090ac822cddc9e6b42
grogachev <rogachev@consol.me>
2022-01-29 06:21:50 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+import WalletconnectProvider from '@walletconnect/web3-provider'
+
+defineProps<{
+  msg: string
+}>()
+
+async function wow() {
+  const config = {97: 'https://data-seed-prebsc-1-s1.binance.org:8545/'}
+  const wc = new WalletconnectProvider({rpc: config})
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://v3.vuejs.org/">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
